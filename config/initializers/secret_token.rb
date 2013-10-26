3536168660f4d15afa72af1218d5334db5f05a9c
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-10-26 12:47:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webify::Application.config.secret_token = '313696843fc83b1711186285ba781137e135983754af65abe6288d70ee5e957cb9575380d0d0472fbfceaf1cd4154aece674072fe17b5938cc8059afa821d3b2'
