a33db6b35e02fc84c9c54d7d05395cc4cea10580
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-10-27 01:48:10 +0200

    Adding the menu step definition and fixing issues with Twitter bootstrap

--- app/models/step_definition.rb
+++ app/models/step_definition.rb
@@ -49,7 +49,17 @@ class StepDefinition
 			self.append_gem("twitter-bootstrap-rails", project_name)
 			
 			# Generating the Twitter Bootstrap layout files
+			system("BUNDLE_GEMFILE=Gemfile bundle exec rails g bootstrap:install static")
 			system("BUNDLE_GEMFILE=Gemfile bundle exec rails g bootstrap:layout application fluid -f")
+
+			# Update the HTML body field
+			body_template = ERB.new(IO.read("#{Rails.root}/templates/body.html.erb"))
+			layout = File.read("app/views/layouts/application.html.erb")
+			layout.gsub!(/<body>(.*)<\/body>/mi, body_template.result(binding))
+
+			File.open("app/views/layouts/application.html.erb", "w+") { |file| file.puts layout }
+
+			File.open("app/assets/stylesheets/bootstrap_and_overrides.css", "a") { |file| file.puts "body { padding-top: 60px; }" }
 
 			# Commiting changes
 			self.commit("Adding Twitter Bootstrap files", project_name)
@@ -98,7 +108,7 @@ class StepDefinition
 			page_names = step.scan(/"([^"]*)"/).flatten
 
 			page_names.each do |page|
-				system("BUNDLE_GEMFILE=Gemfile bundle exec rails g scaffold_controller #{page.downcase.underscore}")
+				system("BUNDLE_GEMFILE=Gemfile bundle exec rails g controller #{page.downcase.underscore} index")
 			end
 
 			# Commiting changes
@@ -108,10 +118,17 @@ class StepDefinition
 
 	def self.add_menu_items(regex, step, project_name)
 		Dir.chdir("#{PROJECTS_PATH}/#{project_name}") do
-			page_names = step.scan(/"([^"]*)"/).flatten
+			link_titles = step.scan(/"([^"]*)"/).flatten
+
+			namespace = OpenStruct.new(link_titles: link_titles)
+			menu_template = ERB.new(IO.read("#{Rails.root}/templates/menu.html.erb"))
+
+			File.open("app/views/layouts/_menu.html.erb", "w+") do |file|
+				file.write(menu_template.result(namespace.instance_eval { binding }))
+			end
 
 			# Commiting changes
-			self.commit("Adding #{page_names.join(' ')} menu items", project_name)
+			self.commit("Adding #{link_titles.join(' ')} menu items", project_name)
 		end
 	end
 
