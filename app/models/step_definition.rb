11f601cdf9f5001d0801e610d6f85405d24319ec
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-10-26 20:53:21 +0200

    Adding step definition for creating a model and adding columns

--- app/models/step_definition.rb
+++ app/models/step_definition.rb
@@ -2,7 +2,9 @@ class StepDefinition
 	# The key is the REGEX and the value is the method to be called
 	MATCHERS = {
 		/I have a website named "(.*?)"$/ => :initialize_project,
-		/I use "(.*?)" as a styling framework$/ => :add_twitter_bootstrap
+		/I use "(.*?)" as a styling framework$/ => :add_twitter_bootstrap,
+		/I have a "(.*?)" table$/ => :add_model,
+		/the "(.*?)" table has a field named "(.*?)" with type "(.*?)"$/ => :add_column
 	}
 
 	PROJECTS_PATH = "../webify_projects"
@@ -46,14 +48,42 @@ class StepDefinition
 			system("BUNDLE_GEMFILE=Gemfile bundle exec rails g bootstrap:layout application fluid -f")
 
 			# Commiting changes
-			git = Git.open("./")
-			git.add
-			git.commit("Adding Twitter Bootstrap files")
+			self.commit("Adding Twitter Bootstrap files", project_name)
+		end
+	end
+
+	def self.add_model(regex, step, project_name)
+		step =~ regex
+
+		Dir.chdir("#{PROJECTS_PATH}/#{project_name}") do
+			system("BUNDLE_GEMFILE=Gemfile bundle exec rails g model #{$1}")	
+
+			# Commiting changes
+			self.commit("Adding model #{$1}", project_name)
+		end
+	end
+
+	def self.add_column(regex, step, project_name)
+		step =~ regex
+
+		Dir.chdir("#{PROJECTS_PATH}/#{project_name}") do
+			system("BUNDLE_GEMFILE=Gemfile bundle exec rails g migration add_#{$2}_to_#{$1} #{$2}:#{$3}")	
+
+			# Commiting changes
+			self.commit("Adding column #{$2} to model #{$1}", project_name)
 		end
 	end
 
 	def self.append_gem(gem_name, project_name)		
 		File.open("Gemfile", "a") { |file| file.puts "gem '#{gem_name}'" }
+
+		# Installing the gem
 		system("BUNDLE_GEMFILE=Gemfile bundle install")
+	end
+
+	def self.commit(message, project_name)
+		git = Git.open("./")
+		git.add
+		git.commit(message)
 	end
 end
